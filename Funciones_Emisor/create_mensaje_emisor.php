229ccf21439828f3a0bad13f27bc363eeccbd541
unknown <branmartinezmedina@gmail.com>
2022-03-18 16:32:31 -0600

    Modificacion

--- Funciones_Emisor/create_mensaje_emisor.php
+++ Funciones_Emisor/create_mensaje_emisor.php
@@ -2,6 +2,7 @@
     include("conexion_emisor.php");
     include("conexion_receptor.php");
     include("conexion_servidor.php");
+
     $Emisor = "5524430586";
     $Receptor = $_POST['txtNumeroContacto'];
     $Mensaje = $_POST['txtMensaje'];
@@ -9,6 +10,7 @@
     $Fecha_Hora = date('m-d-Y h:i:s a', time());
     $Tipo_Mensaje = "S";
     $Estado = "P"; 
+
     $sentenciaEmisor="INSERT INTO mensaje VALUES(        
         DEFAULT,
         '$Emisor',
@@ -17,25 +19,31 @@
         '$Estado', 
         '$Tipo_Mensaje'
         );";
+
+    $sentenciaReceptor="INSERT INTO mensaje VALUES(        
+        DEFAULT,
+        '$Emisor',
+        '$Mensaje',
+        '$Fecha_Hora', 
+        '$Estado', 
+        '$Tipo_Mensaje'
+        );";
+    
+    $sentenciaServidor="INSERT INTO chat_servidor VALUES(        
+        DEFAULT,
+        '$Emisor',
+        '$Receptor',
+        '$Mensaje',
+        '$Fecha_Hora', 
+        '$Estado'
+        );";
     if(mysqli_query($conexion_emisor,$sentenciaEmisor)){ 
-        $sentenciaReceptor="INSERT INTO mensaje VALUES(        
-            DEFAULT,
-            '$Emisor',
-            '$Mensaje',
-            '$Fecha_Hora', 
-            '$Estado', 
-            '$Tipo_Mensaje'
-            );";
+        header("Location: ../emisor.php");
     }
-    if(mysqli_query($conexion_servidor,$sentenciaServidor)){  
-        $sentenciaServidor="INSERT INTO chat_servidor VALUES(        
-            DEFAULT,
-            '$Emisor',
-            '$Receptor',
-            '$Mensaje',
-            '$Fecha_Hora', 
-            '$Estado'
-            );";
+    if(mysqli_query($conexion_receptor,$sentenciaReceptor)){  
+        header("Location: ../emisor.php");
+        
+    }if(mysqli_query($conexion_servidor,$sentenciaServidor)){
         header("Location: ../emisor.php");
     }
     else{
