0bad1923bc404ad483d5e695c80b2deb77a6ab77
Rodrigo <108javierr@gmail.com>
2022-03-19 13:16:10 -0600

    Modificaciones a emisor

--- emisor.php
+++ emisor.php
@@ -26,7 +26,7 @@
         <script>
             function enviar1() {
                 document.forms.formEnviar.action = "./Funciones_Emisor/create_mensaje_emisor.php";
-                document.formEnviar.target = "_blank";
+                document.formEnviar.target = "_self";
                 document.forms.formEnviar.submit()
             }
             function enviar2() {
