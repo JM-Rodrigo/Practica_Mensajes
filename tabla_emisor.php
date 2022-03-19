67d0fa78f3f4346d2cee01597a02933778cefc63
Rodrigo <108javierr@gmail.com>
2022-03-19 16:56:14 -0600

    Modificaciones a receptor

--- tabla_emisor.php
+++ tabla_emisor.php
@@ -42,31 +42,7 @@
                                 <td>".$verChat["Fecha_Hora"]."</td>
                             </tr>";
                         } 
-
-                        $Emisor = "5524430586";
-                        $Mensaje = $verChat["Mensaje"];
-                        $Fecha_Hora = $verChat["Fecha_Hora"];
-                        $Estado = "Pendiente"; 
-                        $Tipo_Mensaje = "E";
                         mysqli_close($conexion);                                       
                 ?>      
                 </tbody>        
             </table>
-            <?php
-                include("conexion_receptor.php");
-                $sentenciaMensaje="INSERT INTO mensaje VALUES(        
-                    DEFAULT,
-                    '$Emisor',
-                    '$Fecha_Hora',
-                    '$Mensaje',
-                    '$Tipo_Mensaje',
-                    '$Estado'
-                    );";
-                if(mysqli_query($conexion1,$sentenciaMensaje)){      
-                    header("Location: ../emisor.php");          
-                }
-                else{
-                    echo "Error";
-                }
-                mysqli_close($conexion1);
-            ?>
