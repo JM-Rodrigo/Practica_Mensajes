701aac31fe283a26cab136fc36515012786dca06
firemachineFTW <61358182+firemachineFTW@users.noreply.github.com>
2022-04-27 16:14:37 -0500

    fokiu

--- tabla.php
+++ tabla.php
@@ -1,47 +1,54 @@
-<table class="table table-striped" id = "tb">
-                <thead>
-                    <tr>
-                        <th scope="col">Nombre</th>
-                        <th scope="col"></th>
-                        <th scope="col"></th>
-                        <th scope="col">Direccion</th>
-                        <th scope="col"></th>
-                        <th scope="col"></th>
-                        <th scope="col">Telefono</th>
-                        <th scope="col"></th>
-                        <th scope="col"></th>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <table class="table table-striped" id = "tb">
+                    <thead>
+                        <tr>
+                            <th scope="col">Nombre</th>
+                            <th scope="col"></th>
+                            <th scope="col"></th>
+                            <th scope="col">Direccion</th>
+                            <th scope="col"></th>
+                            <th scope="col"></th>
+                            <th scope="col">Telefono</th>
+                            <th scope="col"></th>
+                          <th scope="col"></th>
                         <th scope="col">Email</th>
                       
-                </thead>
-                <tbody>
-                <?php
-                    include("./Funciones/conexion.php"); //importar la conexion
-                    $sentencia = "SELECT
-                    proveedor.NombreProveedor, 
-                    proveedor.DireccionProveedor, 
-                    proveedor.TelefonoProveedor, 
-                    proveedor.EmailProveedor
-                    FROM
-                    proveedor";
-                    
-                        $resultado = mysqli_query($conexion, $sentencia);
-                    
-                        while($mostrar = mysqli_fetch_assoc($resultado)){
-                            echo"
-                            <tr>
-                                <td>".$mostrar["NombreProveedor"]."</td>
-                                <td></td>
-                                <td></td>
-                                <td>".$mostrar["DireccionProveedor"]."</td>
-                                <td></td>
-                                <td></td>
-                                <td>".$mostrar["TelefonoProveedor"]."</td>
-                                <td></td>
-                                <td></td>
-                                <td>".$mostrar["EmailProveedor"]."</td>
-                            </tr>";
-                        } 
-                        mysqli_close($conexion);                                       
-                ?>      
-                </tbody>        
-            </table>
+                    </thead>
+                    <tbody>
+                    <?php
+                        include("./Funciones/conexion.php"); //importar la conexion
+                        $sentencia = "SELECT
+                        proveedor.NombreProveedor, 
+                        proveedor.DireccionProveedor, 
+                        proveedor.TelefonoProveedor, 
+                        proveedor.EmailProveedor
+                        FROM
+                       proveedor";
+
+                            $resultado = mysqli_query($conexion, $sentencia);
+
+                            while($mostrar = mysqli_fetch_assoc($resultado)){
+                                echo"
+                                <tr>
+                                   <td>".$mostrar["NombreProveedor"]."</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>".$mostrar["DireccionProveedor"]."</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>".$mostrar["TelefonoProveedor"]."</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>".$mostrar["EmailProveedor"]."</td>
+                                </tr>";
+                            } 
+                            mysqli_close($conexion);                                       
+                    ?>      
+                    </tbody>        
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
